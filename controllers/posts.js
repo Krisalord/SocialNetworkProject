6ee668ca4b255c3b0df8213a2b00975a77831328
Artur Motin <artur.motin98@gmail.com>
2023-04-04 19:31:23 +0300

    Update posts.js

--- controllers/posts.js
+++ controllers/posts.js
@@ -26,7 +26,8 @@ module.exports = {
         try{
             const post = await Post.findById(req.params.id)
             const comments = await Comment.find({post: req.params.id}).sort({createdAt: 'desc'}).lean()
-            res.render('post.ejs', {post: post, user: req.user, comments: comments})
+            const userCreated = await User.findById(post.user)
+            res.render('post.ejs', {post: post, user: req.user, comments: comments, userCreated: userCreated})
         }catch(err){
             console.log(err)
         }
